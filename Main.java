851f80291c2d29a11600d65e112dc03fef64114e
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2021-02-26 14:04:36 +0000

    I added comments

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
 class Main {
   public static void main(String[] args) 
   {
-    System.out.println("You notice that you need turn in assignment soon, but to find it you must search through the downloads file and find /fileX4H3S ");
+    Systemout.println(" ");
+
+    System.out.println("You notice that you need turn in assignment soon, but to find it you must search through the download file and find /fileX4H3S ");//This starts of the quest for looking for files
 
     System.out.println(" ");
 
     int result = fileNum(1);
-    System.out.println("On your laptop you find the " + result + "st download file");
+    System.out.println("On your laptop you find the " + result + "st download file");//A recursive method can be used for searchin files, so I wrote this quick narrative to show how, one file leads into another and keeps branching
 
     System.out.println(" ");
  
     int newResult = fileNume(2);
-    System.out.println("Ater you access the " + result + "st download file, You notice that there are " + newResult + " other download files. Sadly none of them are /fileX4H3S");
+    System.out.println("Ater you access the " + result + "st download file, You notice that there are " + newResult + " other download files. Sadly none of them are /fileX4H3S");//after accesing one file the character finds 3 subfiles
 
     System.out.println(" ");
 
@@ -22,6 +24,7 @@ class Main {
     }else{
       System.out.println("Don't give keep looking for /fileX4H3S");
     }
+    //By choosing one of the three subfiles the charater finds 6 othe files, and this could go on forever. After accessing one of the 6 files he could find 12 new files and so on
 
   }
 
@@ -33,7 +36,7 @@ class Main {
     }else{
       return 0;
     }
-  }
+  }//This is a simple addition recursive method, just like all the other ones below. 
 
 public static int fileNume(int files) 
   {
