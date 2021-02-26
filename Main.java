32b2e6a5b752bcb269f7afd31dd1a7d4966f33db
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2021-02-26 13:58:06 +0000

    I added story and a reason why recursive methods are used

--- Main.java
+++ Main.java
@@ -1,22 +1,24 @@
 class Main {
   public static void main(String[] args) 
   {
+    System.out.println("You notice that you need turn in assignment soon, but to find it you must search through the downloads file and find /fileX4H3S ");
+
     System.out.println(" ");
 
     int result = fileNum(1);
-    System.out.println("In a program you notice a /fileClass" + result);
+    System.out.println("On your laptop you find the " + result + "st download file");
 
     System.out.println(" ");
  
     int newResult = fileNume(2);
-    System.out.println("Ater you access /fileClass" + result + ", You notice that there are " + newResult + " other files");
+    System.out.println("Ater you access the " + result + "st download file, You notice that there are " + newResult + " other download files. Sadly none of them are /fileX4H3S");
 
     System.out.println(" ");
 
     if(6>5)
     {
     int finalResult = fileNumber(3);
-    System.out.println("You then access one of the " + newResult + " new files, and to your suprise you find " + finalResult + " new files.");
+    System.out.println("You then access one of the " + newResult + " other download files, and to your suprise you find /fileX4H3S, amongst " + finalResult + " new files.");
     }else{
       System.out.println("Don't give keep looking for /fileX4H3S");
     }
