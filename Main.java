91c0f862e3d05dba2077005ca59fa20394f7beb1
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2021-02-26 13:23:45 +0000

    I added another method not too different from the first one

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 class Main {
   public static void main(String[] args) 
   {
-    int result = fileNum(8);
-    System.out.println("/fileClass" + result);
+    System.out.println(" ");
+
+    int result = fileNum(1);
+    System.out.println("In a program you notice a /fileClass" + result);
+
+    System.out.println(" ");
+
+    int newResult = fileNume(2);
+    System.out.println("Ater you access /fileClass" + result + ", You notce that there are " + newResult + " other files");
   }
 
   public static int fileNum(int file) 
@@ -14,4 +21,15 @@ class Main {
       return 0;
     }
   }
-}  
+
+public static int fileNume(int files) 
+  {
+    if (files > 0) 
+    {
+      return files + fileNume(files - 1);
+    }else{
+      return 0;
+    }
+  }
+}
+ 
